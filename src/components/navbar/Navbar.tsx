19541bc9835c84287b0cf2fe445da36b485ebe2b
Gabb <nasolgabrielr@gmail.com>
2025-01-08 13:39:44 +0800

    create  navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 z-[40] w-full h-[100px] bg-white flex justify-between items-center px-10 md:px-20">
+      <div className="flex flex-row gap-3 items-center">
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
