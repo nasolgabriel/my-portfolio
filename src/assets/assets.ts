7bee6cd2552c64acf1ee3b487a42d06a6b534b3c
Gabb <nasolgabrielr@gmail.com>
2025-01-14 09:52:59 +0800

    button modification

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,7 +1,8 @@
 import vader_icon from './icons8-darth-vader.svg'
+
 export { default as Brightness4 } from '@mui/icons-material/Brightness4';
 export { default as MenuIcon } from '@mui/icons-material/Menu';
-
+export { default as DownloadIcon } from '@mui/icons-material/DownloadForOffline';
 export const assets = {
     vader_icon
 };
--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -3,18 +3,37 @@
 import { Button } from "@mui/material";
 import React from "react";
 import "../../app/styles/globals.css";
+import { MdOutlineFileDownload } from "react-icons/md";
+
 const Header = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center background-pattern px-5">
-      <div className=" text-center lg:px-20">
-        <h1 className="font-Sans font-bold text-[#111] text-[40px] md:text-[25px] lg:text-[50px] leading-[1.3] mb-2">
+      <div className=" text-center lg:px-20 text-[#111]">
+        <h1 className="font-Sans font-bold text-[40px] md:text-[25px] lg:text-[55px] leading-[1.3] mb-2">
           HELLO THERE, I'M GAB
         </h1>
-        <p className="font-Sans leading-[1.5] lg:text-[20px]">
-          A dedicated and enthusiastic person, eager to kickstart my career
-          in tech, driven by continuous learning and innovation, with a passion
-          for <span className="font-bold">solving problems—one line at a time.</span>
+        <p className="font-Sans leading-[1.5] lg:text-[20px] pb-4">
+          A dedicated and enthusiastic person, eager to kickstart my career in
+          tech, driven by continuous learning and innovation, with a passion for{" "}
+          <span className="font-bold">
+            solving problems—one line at a time.
+          </span>
         </p>
+        <Button
+          className="font-bold py-3 px-6 rounded-lg text-white transition 
+           transform hover:-translate-y-0.5 hover:text-[white]"
+          variant="contained"
+          size="large"
+          style={{
+            borderRadius: "8px",
+            backgroundColor: "#800000",
+            fontWeight: "bold",
+            padding: "10px 20px",
+          }}
+        >
+          my resume{" "}
+          <MdOutlineFileDownload size={23} style={{ marginLeft: "3px", marginBottom:"2px" }} />
+        </Button>
       </div>
     </div>
   );
